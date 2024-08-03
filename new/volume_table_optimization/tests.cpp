7c04f5de6b55fdb76d0a7bf999b8c2e2a57316c1
Electron1997 <mirco.paul.1997@gmail.com>
2024-08-03 14:59:02 +0200

    Volume table optimization

--- new/volume_table_optimization/tests.cpp
+++ new/volume_table_optimization/tests.cpp
@@ -8,7 +8,7 @@
 constexpr size_t D = 30, W = 30;
 
 unoptimized_volume_table<uint32_t, float, D, W, hash32> unoptimized_table;
-optimized_volume_table<uint32_t, float, D, W, hash32> optimized_table;
+unoptimized_volume_table<uint32_t, int, D, W, hash32> optimized_table;
 
 void test_unoptimized_table(int n){
     for(size_t i = 0; i < n; ++i){
@@ -18,11 +18,11 @@ void test_unoptimized_table(int n){
 
 void test_optimized_table(int n){
     for(size_t i = 0; i < n; ++i){
-        optimized_table.update(uniform_key<uint32_t>(0, 10000), normal_volume<float>(0.0, 5.0));
+        optimized_table.update(uniform_key<uint32_t>(0, 10000), uniform_key<int>(0, 10000));
     }
 }
 
 int main(int argc, char *argv[]){
     int n = std::stoi(argv[1]);
-    test_optimized_table(n);
+    test_unoptimized_table(n);
 }
